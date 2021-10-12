b41248181d8946d9c0b6bc40f3488565a72823e8
FaceToKeyboard <fanno.chea@gmail.com>
2021-10-12 16:21:39 -0600

    GET /messages

--- server/app.js
+++ server/app.js
@@ -1,10 +1,21 @@
 import express from 'express';
 import path from 'path';
+import getMessages from '../database/controllers/getMessages';
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(path.resolve('..', 'client', 'dist')));
+
+app.get('/messages', (req, res) => {
+  getMessages(req.query.userId)
+    .then((data) => {
+      res.status(200).send(data);
+    })
+    .catch(() => {
+      res.status(500).send('Internal server error');
+    });
+});
 
 export default app;
